58cc7e066f31f71dc7d6c1c811afc5b4fd48109b
francoisledroff <francois.le.droff@gmail.com>
2014-07-29 15:40:51 +0200

    back to aem name

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer       'Francois Le Droff'
 maintainer_email 'francois.le.droff@gmail.com'
 license          'Apache 2.0'
-name             'cq'
+name             'aem'
 description      'Installs/Configures Adobe CQ/AEM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
